74f393a62b0c5ddbcfc4547b0486ea35f4d7e72f
Rorz <1853310+rorz@users.noreply.github.com>
2023-12-27 16:36:03 +0000

    part 4 boilerplate

---
+++ lib/day04.ex
@@ -0,0 +1,26 @@
+defmodule Day04.Part1 do
+  def solve(input) do
+    nil
+  end
+end
+
+defmodule Day04.Part2 do
+  def solve(input) do
+    nil
+  end
+end
+
+defmodule Mix.Tasks.Day04 do
+  use Mix.Task
+
+  def run(_) do
+    IO.puts("==DAY 04==")
+
+    {:ok, input} = File.read("input/day04.txt")
+
+    IO.puts("*** Part I ***")
+    IO.puts(Day04.Part1.solve(input))
+    IO.puts("*** Part II ***")
+    IO.puts(Day04.Part2.solve(input))
+  end
+end
--- test/day03_test.exs
+++ test/day03_test.exs
@@ -18,6 +18,7 @@ defmodule Day03Test do
     assert Day03.Part1.solve(@example) == 4361
   end
 
+  @tag full: true
   test("solves the full input for part 1") do
     {:ok, input} = File.read("input/day03.txt")
     assert Day03.Part1.solve(input) == 521_515
@@ -27,6 +28,7 @@ defmodule Day03Test do
     assert Day03.Part2.solve(@example) == 467_835
   end
 
+  @tag full: true
   test("solves the full input for part 2") do
     {:ok, input} = File.read("input/day03.txt")
     assert Day03.Part2.solve(input) == 69_527_306
