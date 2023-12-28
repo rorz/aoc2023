65ffab2df26e3edea8ad476446a722d927e37c20
Rorz <1853310+rorz@users.noreply.github.com>
2023-12-28 22:29:16 +0000

    working day 6 part 1

--- lib/day06.ex
+++ lib/day06.ex
@@ -1,6 +1,72 @@
+defmodule Day06.Utils do
+  def get_race_results(input) do
+    Utils.split_into_lines(input)
+    |> Enum.map(&parse_times_from_line/1)
+    |> Enum.zip()
+  end
+
+  defp parse_times_from_line(line) do
+    line
+    |> get_number_part_from_line()
+    |> String.split(" ", trim: true)
+    |> Enum.map(fn num_str ->
+      {num, _} = Integer.parse(num_str)
+      num
+    end)
+  end
+
+  defp get_number_part_from_line(line) do
+    [_, numbers] =
+      line |> String.split(":", trim: true)
+
+    numbers
+  end
+end
+
 defmodule Day06.Part1 do
   def solve(input) do
-    nil
+    race_results = Day06.Utils.get_race_results(input)
+
+    race_results
+    |> Enum.map(&get_winning_range_for_result/1)
+    |> Enum.map(fn range -> Range.size(range) end)
+    |> Enum.product()
+  end
+
+  defp get_winning_range_for_result({time_limit, distance_to_beat}) do
+    lower_bound =
+      stop_at_winning_time(
+        0..time_limit,
+        time_limit,
+        distance_to_beat
+      )
+
+    upper_bound =
+      stop_at_winning_time(
+        time_limit..0,
+        time_limit,
+        distance_to_beat
+      )
+
+    lower_bound..upper_bound
+  end
+
+  defp get_distance_for(ms_button_pressed, time_limit) do
+    go_distance = time_limit - ms_button_pressed
+    go_distance * ms_button_pressed
+  end
+
+  defp stop_at_winning_time(search_range, time_limit, distance_to_beat) do
+    search_range
+    |> Enum.reduce_while(0, fn ms_button_pressed, _ ->
+      case get_distance_for(ms_button_pressed, time_limit) do
+        distance when distance > distance_to_beat ->
+          {:halt, ms_button_pressed}
+
+        _ ->
+          {:cont, ms_button_pressed}
+      end
+    end)
   end
 end
 
