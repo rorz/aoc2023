931b6411b862b7ad170eb51cb6d19228ecdd45a0
Rorz <1853310+rorz@users.noreply.github.com>
2023-12-29 15:38:20 +0000

    set up tests for day 8 part 2

--- test/day08_test.exs
+++ test/day08_test.exs
@@ -1,7 +1,7 @@
 defmodule Day08Test do
   use ExUnit.Case
 
-  @example_first """
+  @example_part1_1 """
   RL
 
   AAA = (BBB, CCC)
@@ -13,7 +13,7 @@ defmodule Day08Test do
   ZZZ = (ZZZ, ZZZ)
   """
 
-  @example_second """
+  @example_part1_2 """
   LLR
 
   AAA = (BBB, BBB)
@@ -23,11 +23,29 @@ defmodule Day08Test do
 
   @tag example: true
   test "solves the first example input for part 1" do
-    assert Day08.Part1.solve(@example_first) == 2
+    assert Day08.Part1.solve(@example_part1_1) == 2
   end
 
   @tag example: true
   test "solves the second example input for part 1" do
-    assert Day08.Part1.solve(@example_second) == 6
+    assert Day08.Part1.solve(@example_part1_2) == 6
+  end
+
+  @example_part2 """
+  LR
+
+  11A = (11B, XXX)
+  11B = (XXX, 11Z)
+  11Z = (11B, XXX)
+  22A = (22B, XXX)
+  22B = (22C, 22C)
+  22C = (22Z, 22Z)
+  22Z = (22B, 22B)
+  XXX = (XXX, XXX)
+  """
+
+  @tag example: true
+  test "solves the example input for part 2" do
+    assert Day08.Part2.solve(@example_part2) == 6
   end
 end
