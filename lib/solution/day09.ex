c0a45626761119f7b020f8d9a2da3fdf335c94a2
Rorz <1853310+rorz@users.noreply.github.com>
2023-12-29 23:01:01 +0000

    correct solution for day 09 part 1

--- lib/solution/day09.ex
+++ lib/solution/day09.ex
@@ -1,6 +1,52 @@
 defmodule Day09.Part1 do
   def solve(input) do
-    nil
+    input
+    |> Utils.split_into_lines()
+    |> Enum.map(&parse_numbers/1)
+    |> Enum.map(&get_difference_lines([&1]))
+    |> Enum.map(fn difference_lines ->
+      difference_lines
+      |> Enum.reduce(0, fn difference_line, acc ->
+        [last_num | _] = difference_line |> Enum.reverse()
+        acc + last_num
+      end)
+    end)
+    |> Enum.sum()
+  end
+
+  defp parse_numbers(line) do
+    line
+    |> String.split(" ", trim: true)
+    |> Enum.map(&Utils.to_int/1)
+  end
+
+  defp get_difference_lines(difference_lines) do
+    [line_to_compute | _] = difference_lines
+
+    {_, difference_line} =
+      line_to_compute
+      |> Enum.reduce({nil, []}, fn curr_num, {prev_num, diffs} ->
+        cond do
+          !prev_num ->
+            {curr_num, []}
+
+          true ->
+            diff = curr_num - prev_num
+            {curr_num, [diff | diffs]}
+        end
+      end)
+
+    new_difference_lines = [
+      difference_line |> Enum.reverse() | difference_lines
+    ]
+
+    if difference_line |> Enum.all?(&is_zero?/1),
+      do: new_difference_lines,
+      else: get_difference_lines(new_difference_lines)
+  end
+
+  defp is_zero?(num) do
+    num == 0
   end
 end
 
--- test/solution/day09_test.exs
+++ test/solution/day09_test.exs
@@ -5,13 +5,14 @@ defmodule Day09Test do
   alias Day09.Part2, as: Part2
 
   @example """
-  EXAMPLE_INPUT_HERE
+  0 3 6 9 12 15
+  1 3 6 10 15 21
+  10 13 16 21 30 45
   """
 
-  @tag :skip
   @tag example: true
   test "solves the example input for part 1" do
-    assert Part1.solve(@example) == nil
+    assert Part1.solve(@example) == 114
   end
 
   @tag :skip
